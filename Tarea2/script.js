a633ce0f6a07c6b0669c896ececcdffda3721f9d
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2021-06-21 23:44:27 -0600

    reflex agent added

---
+++ Tarea2/script.js
@@ -0,0 +1,22 @@
+const reflex_agent = (location, state)=>{
+	if (state=="DIRTY") return "CLEAN";
+   	else if (location=="A") return "RIGHT";
+   	else if (location=="B") return "LEFT";
+}
+
+const test = (states)=>{
+	let location = states[0];		
+	let state = states[0] == "A" ? states[1] : states[2];
+	let action_result = reflex_agent(location, state);
+	document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
+	if (action_result == "CLEAN"){
+		if (location == "A") states[1] = "CLEAN";
+		else if (location == "B") states[2] = "CLEAN";
+	}
+	else if (action_result == "RIGHT") states[0] = "B";
+	else if (action_result == "LEFT") states[0] = "A";
+	setTimeout(()=>{ test(states); }, 2000);	 
+}
+
+
+test(["A","DIRTY","DIRTY"]);
